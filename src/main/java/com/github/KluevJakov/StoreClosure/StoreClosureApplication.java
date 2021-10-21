578b4e278a9e0f10475b6a61614e726908175943
KluevJakov <kluevja@gmail.com>
2021-10-21 15:28:13 +0400

    init commit

---
+++ src/main/java/com/github/KluevJakov/StoreClosure/StoreClosureApplication.java
@@ -0,0 +1,13 @@
+package com.github.KluevJakov.StoreClosure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreClosureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreClosureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/KluevJakov/StoreClosure/StoreClosureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.KluevJakov.StoreClosure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreClosureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
