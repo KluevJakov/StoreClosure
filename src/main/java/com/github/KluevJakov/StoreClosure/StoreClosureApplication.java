0f90aaafbce63e75a7d4f2819f6a899fba6b70d1
KluevJakov <kluevja@gmail.com>
2021-10-21 18:12:09 +0400

    db configuration

--- src/main/java/com/github/KluevJakov/StoreClosure/StoreClosureApplication.java
+++ src/main/java/com/github/KluevJakov/StoreClosure/StoreClosureApplication.java
@@ -1,9 +1,12 @@
 package com.github.KluevJakov.StoreClosure;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class StoreClosureApplication {
 
 	public static void main(String[] args) {
